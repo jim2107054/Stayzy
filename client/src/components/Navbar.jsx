e82a23b2a8a90ab51c8812b2c4493436daeaed28
jim2107054 <mdjahid205hasan@gmail.com>
2025-05-08 23:04:45 +0600

    my booking in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { assets } from './../assets/assets';
 import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
+
+const BookIcon = ()=>(
+  <svg className="w-4 h-4 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
+  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4" />
+</svg>
+)
 
 const Navbar = () => {
   const navLinks = [
@@ -11,13 +17,14 @@ const Navbar = () => {
     { name: "About", path: "/" },
   ];
 
-  const navigate = useNavigate();
-
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const {openSignIn} = useClerk();
   const {user} = useUser();
+
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -74,9 +81,19 @@ const Navbar = () => {
       {/* Desktop Right */}
       <div className="hidden md:flex items-center gap-4">
         <img src={assets.searchIcon} alt="Search" className={`${isScrolled && 'invert'} h-7 transition-all duration-500`} />
-        <button onClick={openSignIn} className="bg-gray-800 text-white font-light px-8 py-2.5 rounded-lg ml-4 hover:scale-105 transition-all duration-500">
+        {
+          user ? (
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action label="My Bookings" labelIcon={<BookIcon/>} onClick={()=>navigate('/my-bookings')}/>
+              </UserButton.MenuItems>
+            </UserButton>
+          ):(
+            <button onClick={openSignIn} className="bg-gray-800 text-white font-light px-8 py-2.5 rounded-lg ml-4 hover:scale-105 transition-all duration-500">
           Login
         </button>
+          )
+        }
       </div>
 
       {/* Mobile Menu Button */}
